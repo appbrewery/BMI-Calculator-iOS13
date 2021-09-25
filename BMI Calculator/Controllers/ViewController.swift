6dd1e080459a2f9cfcebc058bcf4b6a6c1c3970a
m00racle <yanuar.prakosa@gmail.com>
2021-09-25 07:27:04 +0700

    link UISlider to each UIlabel

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -9,10 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var heightSlider: UISlider!
+    @IBOutlet weak var weightSlider: UISlider!
+    
+    @IBAction func heightSliding(_ sender: UISlider) {
+        putLabel()
+    }
+    
+    @IBAction func weightSliding(_ sender: UISlider) {
+        putLabel()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        putLabel()
+    }
+    
+    func putLabel() {
+        heightLabel.text = String(format: "%.2f", heightSlider.value) + "m"
+        weightLabel.text = String(format: "%.0f", weightSlider.value) + "kg"
     }
 
 
