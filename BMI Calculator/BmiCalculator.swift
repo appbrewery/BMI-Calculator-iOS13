cf52ddfa4a3a8135e701aa987a895ea47ac6397c
m00racle <yanuar.prakosa@gmail.com>
2021-10-07 07:51:47 +0700

    make BmiCalculator protocol

---
+++ BMI Calculator/BmiCalculator.swift
@@ -0,0 +1,15 @@
+//
+//  BmiCalculator.swift
+//  BMI Calculator
+//
+//  Created by Yanuar Heru on 07/10/21.
+//  Copyright © 2021 Angela Yu. All rights reserved.
+//
+
+import Foundation
+protocol BmiCalculator {
+    var bmi: Float? {get set}
+    
+    mutating func calculateBmi(weight:Float, height:Float) -> Float
+    func putBmi() -> String
+}
